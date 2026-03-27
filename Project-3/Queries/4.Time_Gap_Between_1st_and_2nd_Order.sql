be69571f5dc363bc7bc2d1686888ebc45ec1bb2a
Ravula Naga Karthik <nagakarthikravula@gmail.com>
2026-03-27 10:15:36 +0530

    Update 4.Time_Gap_Between_1st_and_2nd_Order.sql

--- Project-3/Queries/4.Time_Gap_Between_1st_and_2nd_Order.sql
+++ Project-3/Queries/4.Time_Gap_Between_1st_and_2nd_Order.sql
@@ -1,6 +1,19 @@
 Q4: Time Gap Between 1st and 2nd Order
+Tables: olist_order_dataset,olist_customers_dataset
+Approach: Partitioned row_number() by customer_unique_id
+          in cte, Used LEAD() to get 2nd order date,
+          Calculated days gap between 1st order and 2nd order
+Concepts: CTE, ROW_NUMBER(),LEAD(), <> '', Mutliple Table Join
 
+Why is this useful in business?
 
+Understand how quickly customers return
+Helps set timing for re-engagement email campaigns
+If average gap is 45 days, send a reminder on day 40
+Key metric for customer loyalty analysis
+
+***Query***
+  
 with cte as(
 select ocd.customer_unique_id,ood.order_delivered_customer_date,
 lead(ood.order_delivered_customer_date ) over(partition by ocd.customer_unique_id order by ood.order_delivered_customer_date )
